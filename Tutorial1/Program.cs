a848227f2bd577a3765723f4a116acfb0293b342
jpatel207 <justinhpatel@gmail.com>
2020-10-29 20:10:26 -0700

    Week 1 lecture solution

---
+++ Tutorial1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tutorial1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var inputBase10Array = new int []{ 1, 3, 5, 8, 90};
+            foreach(var item in inputBase10Array)
+            {
+                Console.WriteLine(item);
+                ConvertToOctal(item);
+            }
+        }
+
+        private static void ConvertToOctal(int item)
+        {
+            var octalDigits = new List<String>();
+            if (item < 7)
+            {
+                Console.WriteLine($"{item}");
+                return;
+            }
+
+
+
+            var result = 34 / 8;
+            var remainder = item - result * 8;
+            octalDigits.Add();
+            Console.WriteLine($" 1StPositionOctal = {result} Remainder = {remainder}");
+
+
+            var displayOctal = String.Join("", octalDigits);
+            Console.WriteLine($"{displayOctal}");
+           
+        }
+    }
+}
---
+++ Week1_DecimalNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+
+namespace Week1_DecimalNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var decimalNumber = 239;
+
+            // Step one you need to get each digit
+            // The first step is to convert it to value to string -> character
+            // So that you can get to each character 2 3 and 9
+
+            var characterArray = decimalNumber.ToString().ToCharArray();
+            var result = 0;
+            var maxpower = characterArray.Length;
+            var counter = 0;
+            // Test to see if this is what you want
+            foreach(var item in characterArray)
+            {
+                // find length of characterArray, make the first index the highest power and works backwards
+                var digit = int.Parse(item.ToString());
+                var power = Math.Pow(10, maxpower - ++counter);
+                result = (int)(digit * power);
+
+                // now print each character to console
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(result);
+
+        }
+    }
+}
