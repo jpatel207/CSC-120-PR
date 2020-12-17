b603925de1fb75719f48f37cdab001afe14290e3
jpatel207 <justinhpatel@gmail.com>
2020-12-17 03:34:51 -0700

    relocate source code

--- Week7/Program.cs
+++ Week7/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using SpiceSharp;
+using SpiceSharp.Components;
+using SpiceSharp.Simulations;
 
-namespace Week7
+namespace SpiceSimulation
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Build the circuit
+            var ckt = new Circuit(
+                new VoltageSource("V1", "in", "0", 0.0),
+                new Resistor("R1", "in", "out", 1.0e3),
+                new Resistor("R2", "out", "0", 2.0e3)
+                );
+
+            // Create a DC simulation that sweeps V1 from -1V to 1V in steps of 100mV
+            var dc = new DC("DC 1", "V1", -1.0, 1.0, 0.2);
+
+            // Create exports
+            var inputExport = new RealVoltageExport(dc, "in");
+            var outputExport = new RealVoltageExport(dc, "out");
+            var currentExport = new RealPropertyExport(dc, "V1", "i");
+
+            // Catch exported data
+            dc.ExportSimulationData += (sender, argsexportDataEventArgs) =>
+            {
+                var input = inputExport.Value;
+                var output = outputExport.Value;
+                //var current = currentExport.Value;
+            };
+            dc.Run(ckt);
         }
     }
 }
